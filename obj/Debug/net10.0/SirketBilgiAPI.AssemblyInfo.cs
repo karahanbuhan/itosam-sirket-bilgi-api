fc2bc4ccae913e92820f933ffff1daed928d1588
Karahan Buhan <contact@karahanbuhan.com>
2026-06-30 11:04:43 +0300

    Scrapper read book value and market value

--- obj/Debug/net10.0/SirketBilgiAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/SirketBilgiAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SirketBilgiAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7047c7bee8ec09c989996d465d2e03cfbf98a4c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad74bba4d7956816556dc6bdb71745c208ae4fc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SirketBilgiAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SirketBilgiAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Step 1: Get names of pages and urls
+
 res = requests.get("https://www.borsaningundemi.com/piyasa-ekrani/bist-hisseler")
 soup = BeautifulSoup(res.content, "html.parser")
 
@@ -17,4 +19,27 @@ for li in soup.find_all("li"):
             urls.append("https://www.borsaningundemi.com/piyasa-ekrani/hisse-detay/" + c)
 
 
-print(urls)
+# Step 2: Get each page market value and book value
+
+#for url in urls:
+res = requests.get(urls[0])
+soup = BeautifulSoup(res.content, "html.parser")
+
+buff = []
+a = soup.find_all("li")
+for q in a:
+    if q.get("class") is None:
+        continue
+    buff.append(q)
+
+for b in buff:
+    b = str(b)
+    if "c7" in b:
+        b = b.replace('<li class="c7"><p>Piyasa Değeri</p><span><b>TL</b><b>USD</b><small>', "")
+        b = b.replace('</small><small>157.260.638,30</small></span></li>', "")
+    if "Defter Değeri<" in b and "s3" in b:
+        b = b.replace('<li class="s3"><p>Defter Değeri</p><span>', "")
+        b = b.replace("</span></li>", "")
+        print(b)
+
+print("new Company("TÜPRAŞ - Türkiye Petrol Rafinerileri A.Ş.", new DateOnly(2015, 3, 18), 422.35, 183.55)")
